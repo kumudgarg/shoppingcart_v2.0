e057a89c46efc5800a3456e569679baa09c58770
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-04-20 09:52:51 +0530

    2.1:Red: Added multiple product into cart to return totalPrice

--- src/main/java/ProductType.java
+++ src/main/java/ProductType.java
@@ -1,3 +1,3 @@
 public enum ProductType {
-    APPLE
+    MASK, APPLE
 }
--- src/test/java/ShoppingCartTest.java
+++ src/test/java/ShoppingCartTest.java
@@ -20,4 +20,16 @@ public class ShoppingCartTest {
         double totalPrice = shoppingCart.getTotalPrice(quantity);
         Assert.assertEquals(4.95, totalPrice, 0.0);
     }
+
+    @Test
+    public void shouldReturnTotalPriceWhenMultipleProductAdded() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product apple = new Product(ProductType.APPLE);
+        shoppingCart.getAddedToCart(apple);
+        shoppingCart.getTotalPrice(3);
+        Product mask = new Product(ProductType.MASK);
+        shoppingCart.getAddedToCart(mask);
+        double totalPrice = shoppingCart.getTotalPrice(3);
+        Assert.assertEquals(8.94, totalPrice, 0.0);
+    }
 }
