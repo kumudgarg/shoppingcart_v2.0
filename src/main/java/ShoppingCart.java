2f91cc0b9c7e7228924af79124accc32cd9b29b6
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-04-21 11:49:15 +0530

    Refactored: code to handle sales tax

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,32 +7,22 @@ import java.util.stream.Stream;
 
 public class ShoppingCart {
 
-    private boolean flag;
-
-     List<Product> products;
+    List<Product> products;
 
     private SalesTax salesTax;
 
-    private DecimalFormat df;
-
-    private final String PATTERN = "#.##";
+    private BigDecimal bigDecimal;
 
     private double totalPrice = 0.0;
 
-    private int limit;
-
-    private int quantity;
 
     public ShoppingCart() {
-        this.flag = false;
         this.products = new ArrayList<>();
         this.salesTax = new SalesTax();
-        this.df = new DecimalFormat(this.PATTERN);
     }
 
     public void addToCart(Product product, int quantity) throws ShoppingCartException {
-        this.quantity = quantity;
-        if(product.getProductType() == null){
+        if (product.getProductType() == null) {
             throw new ShoppingCartException("null product type", ShoppingCartException.ExceptionType.NULL_PRODUCT_TYPE);
         }
         for (int i = 0; i < quantity; i++)
@@ -40,21 +31,20 @@ public class ShoppingCart {
 
 
     public double getTotalPrice() throws ShoppingCartException {
-        if(products.isEmpty()){
+        if (products.isEmpty()) {
             throw new ShoppingCartException("Empty Product List", ShoppingCartException.ExceptionType.EMPTY_PRODUCTS);
         }
-
         totalPrice = this.totalPrice + products.stream().mapToDouble(product -> product.getProductPrice()).sum();
         return totalPrice;
-
-//
-//        }
-
-//    public String getTotalPriceWithsalesTax(double totalPrice){
-//        double grandTotal = totalPrice + salesTax.getSalesTax(totalPrice);
-//        String format = df.format(grandTotal);
-//        return format;
-//    }
-
     }
+
+    public double getTotalPriceWithSalesTax() throws ShoppingCartException {
+        double totalPrice = getTotalPrice();
+        double grandTotal = this.totalPrice + salesTax.getSalesTax(totalPrice);
+        double precisedTotal = Double.parseDouble(String.format("%.2f", grandTotal));;
+        return precisedTotal;
+    }
+
+
 }
+
--- src/test/java/ShoppingCartTest.java
+++ src/test/java/ShoppingCartTest.java
@@ -40,7 +40,6 @@ public class ShoppingCartTest {
     @Test
     public void shouldThrowCustomExceptionWhenAListOfProductEmpty() throws ShoppingCartException {
         try {
-
             ShoppingCart shoppingCart = new ShoppingCart();
             Product apple = new Product(ProductType.APPLE, 0.99);
             double totalPrice = shoppingCart.getTotalPrice();
@@ -48,17 +47,16 @@ public class ShoppingCartTest {
             Assert.assertEquals(ShoppingCartException.ExceptionType.EMPTY_PRODUCTS, ex.type);
         }
     }
-}
 
-    /*@Test
+
+    @Test
     public void shouldThrowCustomExceptionWhenNullProductTypeAdded() throws ShoppingCartException {
         try {
             int quantity = 5;
             ShoppingCart shoppingCart = new ShoppingCart();
             Product apple = new Product(null, 0.99);
-            shoppingCart.getAddedToCart(apple);
-        }
-        catch (ShoppingCartException ex){
+            shoppingCart.addToCart(apple, 3);
+        } catch (ShoppingCartException ex) {
             Assert.assertEquals(ShoppingCartException.ExceptionType.NULL_PRODUCT_TYPE, ex.type);
 
         }
@@ -69,17 +67,16 @@ public class ShoppingCartTest {
     public void shouldReturnTotalPriceWithSalesTaxWhenMultipleProductAdded() throws ShoppingCartException {
         ShoppingCart shoppingCart = new ShoppingCart();
         Product apple = new Product(ProductType.APPLE, 0.99);
-        shoppingCart.getAddedToCart(apple);
-        shoppingCart.getTotalPrice(2);
-        shoppingCart.getAddedToCart(apple);
-        shoppingCart.getTotalPrice(1);
+        shoppingCart.addToCart(apple, 2);
+        shoppingCart.addToCart(apple,1);
         Product mask = new Product(ProductType.MASK, 1.99);
-        shoppingCart.getAddedToCart(mask);
-        double totalPrice = shoppingCart.getTotalPrice(3);
-        String totalPriceWithsalesTax = shoppingCart.getTotalPriceWithsalesTax(totalPrice);
-        Assert.assertEquals("9.12", totalPriceWithsalesTax);
+        shoppingCart.addToCart(mask, 3);
+        double totalPriceWithsalesTax = shoppingCart.getTotalPriceWithSalesTax();
+        Assert.assertEquals(9.12, totalPriceWithsalesTax, 0.0);
     }
-    @Test
+}
+
+   /* @Test
     public void shouldReturnTotalPriceWithSalesTaxWhenMultipleProductAddedIntoCart() throws ShoppingCartException {
         List<Product> products = new ArrayList<>();
         ShoppingCart shoppingCart = new ShoppingCart();
@@ -99,5 +96,3 @@ public class ShoppingCartTest {
         Assert.assertEquals("9.12", totalPriceWithsalesTax);
     }
 */
-
-
